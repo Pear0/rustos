e384a9e0ffb965ee27df6afae3d27447d06a3f72
wgulian3 <wgulian@gatech.edu>
2020-12-17 20:10:00 -0500

    Create cargo workspace manifest and implement rudimentary ARM semihosting calls

--- lib/aarch64/src/semi.rs
+++ lib/aarch64/src/semi.rs
@@ -6,13 +6,18 @@
 
 unsafe fn do_call(op: u32, param: u64) -> u64 {
     let mut out: u64 = 0;
-    llvm_asm!("hlt #0x3C" : "={x0}"(out) : "{w0}"(op), "{x1}"(param) : "memory" : "volatile" );
+    llvm_asm!("hlt #0xF000" : "={x0}"(out) : "{w0}"(op), "{x1}"(param) : "memory" : "volatile" );
     out
 }
 
+pub fn sys_writec(u: u8) {
+    let u: u64 = u as u64;
+    unsafe { do_call(0x03, (&u) as *const _ as u64) };
+}
 
-
-
+pub fn sys_time() -> u64 {
+    unsafe { do_call(0x11, 0) }
+}
 
 
 
